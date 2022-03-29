1028b21f2e7e3d4776a82520fc12bec35a4583df
wiut9135 <wiut00009135@gmail.com>
2022-03-29 15:41:15 +0500

    Starting the Coursework 2 on Web Technology

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(PORT, (err) => {
+    if (err) throw err
+
+    console.log('This app is running on port' + PORT)
+})
